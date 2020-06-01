5c8bdb2b5de00061dec23c41642f2a3d0dd7f5ae
SMJ <solomonmbak@gmail.com>
2020-06-01 15:22:10 +0100

    Add files via upload

---
+++ myModule.py
@@ -0,0 +1,29 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+var1 = "This is a module"
+var1
+
+
+# %%
+# Area of a triangle
+def triangleArea(base, height):
+    return (1/2) * (base * height)   
+
+
+# %%
+# Area of a Circle
+def circleArea(r):
+    return 3.14 * (r**2)
+
+
+# %%
+# Area of a square
+def squareArea(length):
+    return length * length
+
+
+# %%
+
+
+
